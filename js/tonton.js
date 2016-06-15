fe3842b5afe5674983c57d2a23cb7ccc17349b57
Keita Urashima <ursm@ursm.jp>
2016-06-15 14:41:01 +0900

    Fix typo

--- js/tonton.js
+++ js/tonton.js
@@ -3,7 +3,7 @@ var addTontonButton = function(){
     if (!$('.message-form textarea').length) { return; }
       if ($('#tonton').length == 0){
 	  $('.message-form textarea').parent().append('<label id="tonton" class="ember-view file-select-button btn tooltipstered"><img src="'+chrome.extension.getURL('images/btn-bell.png')+'"></label>')
-	  var contents = '<div id="tonton_list" class="modal fade" tabindex="-1" role="dialog"aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">To whom you knock?</h4></div><div class="modal-body">'
+	  var contents = '<div id="tonton_list" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">To whom you knock?</h4></div><div class="modal-body">'
 	  _.each(_.range(0, $("div.fit:contains('Members')").parent().next().children().length-1), function(num){
 	      username = $.trim($("li.horizontal-container .fit").eq(num).text());
 	      contents += '<img class="tontee" src="'+$("li.horizontal-container").eq(num).find("img").attr("src")+'" text="@'+username+' トントン https://appear.in/'+username+'"></img> '
